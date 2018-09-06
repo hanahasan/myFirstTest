a8bdc113f286f2c007dfa60723dbc7acfca7b79c
Hana Hasan <hahasan@stu.naperville203.org>
2018-09-06 11:46:47 -0500

    added bluej project

---
+++ againAnotherTest/Program1.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class Program1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Program1
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public static void main (String [] args)
+    {
+        System.out.print("Hello");
+    }
+}
