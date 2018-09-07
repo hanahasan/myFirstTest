56076e678a2b025bdce96017834bec495f5efaf3
Hana Hasan <hahasan@stu.naperville203.org>
2018-09-07 12:19:24 -0500

    First addition to parking

---
+++ againAnotherTest/parking.java
@@ -0,0 +1,24 @@
+import java.util.Scanner; 
+import java.util.Stack;
+/**
+ * Write a description of class parking here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ * 
+ * activity 15.10 on pg 715
+ */
+public class parking
+{
+    public static void main (String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        Stack street = new Stack(); 
+        Stack driveway = new Stack(); 
+        
+        System.out.println("What car do you want to add/remove?"); 
+        Integer car = new Integer(in.nextInt());
+       
+        
+    }
+}
