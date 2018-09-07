2c30a7b0b0d6779ded94efb5eb0ab043d206fc98
Hana Hasan <hahasan@stu.naperville203.org>
2018-09-07 12:04:33 -0500

    New fake activity

---
+++ againAnotherTest/activityFake.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class activityFake here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class activityFake
+{
+
+    /**
+     * Constructor for objects of class activityFake
+     */
+    public static void main (String[] args)
+    {
+        System.out.print("this is fake");
+    }
+}
