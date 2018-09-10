57d7a509baff0daf7054e87cfc408748e2dc99ec
Hana Hasan <hahasan@stu.naperville203.org>
2018-09-10 12:13:57 -0500

    Monday -> worked on parking

--- againAnotherTest/parking.java
+++ againAnotherTest/parking.java
@@ -1,5 +1,5 @@
 import java.util.Scanner; 
-import java.util.Stack;
+import java.util.*;
 /**
  * Write a description of class parking here.
  * 
@@ -16,8 +16,34 @@ public class parking
         Stack street = new Stack(); 
         Stack driveway = new Stack(); 
         
-        System.out.println("What car do you want to add/remove?"); 
-        Integer car = new Integer(in.nextInt());
+        Integer car = 1;
+        while (car != 0)
+        {
+            System.out.println("What car do you want to add/remove?"); 
+            car = in.nextInt();
+        
+            if (car < 0) //removing car 
+            
+               if (driveway.peek().intValue() == Math.abs(car.intValue()))//found the car you wanna remove
+               {
+                   driveway.pop();
+                   while (street.empty() == false)//emptying street into driveway
+                   {
+                       driveway.push(street.pop());
+                    }
+                }
+               else
+               {
+                   street.push(driveway.pop());
+                }
+            
+            else //adding car 
+            {
+                driveway.push(car); 
+            }
+            System.out.println("On the Street: " + street); 
+            System.out.println("On the Driveway: " + driveway);
+        }
        
         
     }
