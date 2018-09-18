f874de161749d8c2ba8efd6cc4b03061ade30241
hanahasan <31442052+hanahasan@users.noreply.github.com>
2018-09-18 11:33:41 -0500

    gagh

--- againAnotherTest/UniqueWords.java
+++ againAnotherTest/UniqueWords.java
@@ -1,5 +1,8 @@
 
 import java.util.Scanner; 
+import java.util.Set; 
+import java.util.TreeSet;
+import java.io.*;
 /**
  * Write a description of class TreeSet here.
  * 
@@ -9,8 +12,37 @@ public class UniqueWords
 {
     public static void main (String[] args)
     {
-        Scanner scan = new Scanner();
         
+        try (Scanner scan = new Scanner(new File("ObamaSpeech.txt"));)
+        {
+           Set <String> uniqueWords = new TreeSet<>();
+           while (scan.hasNext())
+           {
+              // String word = clean(scan.next());
+               //System.out.println(word);
+               uniqueWords.add(clean(scan.next())); 
+           }
+           for (String word: uniqueWords)
+           {
+               System.out.println(word);
+            }
+           
+        }
+        catch (FileNotFoundException e){}
     }
+    
+   public static String clean(String s)
+   {
+      String r = "";
+      for (int i = 0; i < s.length(); i++)
+      {
+         char c = s.charAt(i);
+         if (Character.isLetter(c))
+         {
+            r = r + c;
+         }
+      }
+      return r.toLowerCase();
+   }
 
 }
