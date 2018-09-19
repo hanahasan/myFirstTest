f20255bc7d70d10379312d3f83548c1eba929bc5
hanahasan <31442052+hanahasan@users.noreply.github.com>
2018-09-19 12:48:41 -0500

    FINISH FLOODFILL

--- againAnotherTest/Grid.java
+++ againAnotherTest/Grid.java
@@ -20,11 +20,56 @@ public class Grid
        Pair coordinate = new Pair(row,column); 
        Stack <Pair> coordinates = new Stack<Pair>();
        coordinates.push(coordinate);
+       Pair northCord, southCord, eastCord, westCord;
        while (count <=100) 
        {
            Pair compare = coordinates.pop();
-           if (coordinate.getValue() == 0)//not been filled
-           {}
+           if (compare.getValue() == 0)//not been filled
+           {
+               compare.setValue(count);
+               pixels [compare.getRow()][compare.getColumn()] = compare.getValue();
+               count++;
+               coordinates.push(compare);//pushing the center coordinate
+               //checking the north coordinate
+               northCord = new Pair(row+1, column); 
+               if (pixels [northCord.getRow()][northCord.getColumn()] == 0) //empty
+               {
+                   northCord.setValue(count); 
+                   pixels [northCord.getRow()][northCord.getColumn()] = northCord.getValue();
+                   count++;
+                   coordinates.push(northCord); 
+                }
+               //checking east coordinate 
+               eastCord = new Pair(row, column+1);
+               if (pixels [eastCord.getRow()][eastCord.getColumn()] == 0)
+               {
+                   eastCord.setValue(count); 
+                   pixels [eastCord.getRow()][eastCord.getColumn()] = eastCord.getValue();
+                   count++; 
+                   coordinates.push(eastCord); 
+                }
+               //checking south coordinate 
+               southCord = new Pair(row-1, column);
+               if (pixels [southCord.getRow()][southCord.getColumn()] == 0)
+               {
+                   southCord.setValue(count); 
+                   pixels [southCord.getRow()][southCord.getColumn()] = southCord.getValue();
+                   count++; 
+                   coordinates.push(southCord); 
+                }
+               //checking east coordinate 
+               westCord = new Pair(row, column-1);
+               if (pixels [westCord.getRow()][westCord.getColumn()] == 0)
+               {
+                   westCord.setValue(count); 
+                   pixels [westCord.getRow()][westCord.getColumn()] = westCord.getValue();
+                   count++; 
+                   coordinates.push(westCord); 
+                }
+               //reseting the row and column values 
+               row = coordinates.peek().getRow(); 
+               column = coordinates.peek().getColumn();
+            }
        }
 
    }
